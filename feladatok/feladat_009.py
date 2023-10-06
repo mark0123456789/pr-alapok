2a530896e72513629ea277cfec7ddaee99af675e
mark <113088634+mark0123456789@users.noreply.github.com>
2023-10-06 13:24:04 +0200

    Add files via upload

---
+++ feladatok/feladat_009.py
@@ -0,0 +1,36 @@
+# feladat_009
+"""
+Kérjünk be két egész számot, szám1 és szám2
+hasonlitsuk össze a két számot, és irassuk ki, hogy
+a szám1 kisebb mint a szám2, vagy a szám2 kisebb mint a szám1, vagy a szám1 egyenlő
+szám2-vel.
+"""
+szam1 = input("Írj be egy egész számot:")
+szam2 = input("Írj be még egy egész számot:")
+szam1 = int(szam1)
+szam2 = int(szam2)
+#--------------------------------------
+"""
+if szam1 < szam2:
+    print(f"A szám1 kisebb mint a szám2")
+elif szám2< szám1:
+    print(f"A szám2 kisebb mint szám1")
+else:
+    print(f"A szám1 egyenlő a szám2-vel")
+"""
+#--------------------------------------
+"""
+if szam1 < szám2:
+    print(f"A szám1 kisebb mint a szám2")
+if szám2 < szám1
+    print(f"A szám2 kisebb mint a szám1")
+if szám1 == szám2:
+    print(f"A szám1 engyenlő a szám2-vel")
+"""
+#--------------------------------------
+if szam1 > szam2:
+    print(f"szam1 nagyobb mint szam2")
+elif szam1 < szam2:
+    print(f"szam1 kisebb mint szam2")
+elif szam1 == szam2:
+    print(f"szam1 egyenlő szam2-vel")
