76fb134de8c7e0be8d314f5da65fab647dff603b
mark <113088634+mark0123456789@users.noreply.github.com>
2023-11-17 13:10:33 +0100

    Add files via upload

---
+++ feladatok/feladat_019.py
@@ -0,0 +1,9 @@
+#feladat_019
+sor = 1
+while sor <= 4:
+    oszlop = 1
+    while oszlop <= 7:
+        print('0 ',end='')
+        oszlop = oszlop + 1
+    print('')
+    sor = sor +1
---
+++ feladatok/feladat_020.py
@@ -0,0 +1,9 @@
+# a kiírástkövetően a kurzor egy tabulátornyit ugrik
+print('Teszt', end='\t')
+print('Teszt', end='\t')
+print('Teszt', end='\t')
+
+# a kiírást követően a kurzor a kiírás végén marad
+print('Teszt', end='') 
+print('Teszt', end='')
+print('Teszt', end='') 
